ef6026cec53aa13ca99477e06c98c7a0f6e036a5
Łukasz Mrozek <morskk@gmail.com>
2019-05-05 15:34:35 +0200

    Add type to represent position in grid
    The type is called `Position`.
    It is not possible to create a `Position` instance with invalid coordinates.
    Valid coordinates are non-negative and less than `GridSettings.GRID_SIZE`.
    It is going to replace all uses of row and column parameters used together.

---
+++ app/src/main/java/com/example/sudoku/com/example/sudoku/model/Position.java
@@ -0,0 +1,49 @@
+package com.example.sudoku.com.example.sudoku.model;
+
+import java.util.Objects;
+
+public final class Position {
+    private final int row;
+    private final int column;
+
+    public Position(int row, int column) {
+        if (row < 0
+                || row >= GridSettings.GRID_SIZE
+                || column < 0
+                || column >= GridSettings.GRID_SIZE)
+            throw new IllegalArgumentException(
+                    String.format("Row and column must be a value between 0 and %d", GridSettings.GRID_SIZE));
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row &&
+                column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "row=" + row +
+                ", column=" + column +
+                '}';
+    }
+}
